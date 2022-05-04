2c22b3d72a45eeac78647264087d8c2c49ea6093
GausMaxwell <umut.polat.272@gmail.com>
2022-05-04 12:04:59 +0300

    First Commit

---
+++ Avarage.js
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Avarage {
+    public static void main(String[] args) {
+
+        Scanner les=new Scanner(System.in);
+
+        //Math,Physic,Chemical,Turkish,Music,History average point
+
+        int Math,Physic,Chemical,Turkish,Music,History;
+
+        System.out.print("Math:");
+        Math=les.nextInt();
+
+        System.out.print("Physic:");
+        Physic=les.nextInt();
+
+        System.out.print("Chemical:");
+        Chemical=les.nextInt();
+
+        System.out.print("Turkish:");
+        Turkish=les.nextInt();
+
+        System.out.print("Music:");
+        Music=les.nextInt();
+
+        System.out.print("History:");
+        History=les.nextInt();
+
+        double Average;
+        Average=(Math+Physic+Chemical+Turkish+Music+History)/6;
+        System.out.println("Avarege: "+Average);
+
+        String Final=(Average>=50)? "Passed" : "Remain";
+        System.out.print("Final: "+Final);
+
+    }
+}
