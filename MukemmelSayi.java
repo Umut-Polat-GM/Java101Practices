7eff501cdd477b5b3f551df28eb441c2d1a4ed06
Umut Polat <62594144+Umut-Polat-GM@users.noreply.github.com>
2022-06-07 22:12:14 +0300

    Create MukemmelSayi.java

---
+++ MukemmelSayi.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MukemmelSayi {
+    public static void main(String[] args) {
+        int sayi,toplam=0;
+        Scanner input=new Scanner(System.in);
+        System.out.print("Bir sayi giriniz:");
+        sayi=input.nextInt();
+
+        for (int i=1;i<sayi;i++){
+
+            if (sayi%i==0){
+                toplam+=i;
+            }
+        }
+        if (sayi==toplam){
+            System.out.println(sayi+" Mukemmmel sayidir.");
+        }else{
+            System.out.println(sayi+" Mukemmel sayi degildir.");
+        }
+    }
+}
